1f80a31f77d037ff88bef47087cc4db15a36d14e
viaunillahitasya <127423992+viaunillahitasya@users.noreply.github.com>
2023-04-03 00:39:08 +0700

    Add files via upload

---
+++ latihan3/polystatis.py
@@ -0,0 +1,23 @@
+#contoh 1
+print(len("Hello")) # output: 5
+print(len([1, 2, 3])) # output: 3
+print(len((1, 2, 3))) # output: 3
+
+#contoh 2
+print(2 + 3) # output: 5
+print("Hello" + " World") # output: Hello World
+print([1, 2] + [3, 4]) # output: [1, 2, 3, 4]
+
+#contoh 3
+print(max(2, 5)) # output: 5
+print(max([1, 2, 3])) # output: 3
+print(max("Hello")) # output: "o"
+
+#contoh 4
+a = [3, 1, 2]
+a.sort()
+print(a) # output: [1, 2, 3]
+b = ["c", "a", "b"]
+b.sort()
+print(b) # output: ["a", "b", "c"]
+
